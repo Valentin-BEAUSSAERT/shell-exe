8d10041f05494d6982280ea376274c738ffbe53e
Valentin-BEAUSSAERT <valentin.beaussaert@laplateforme.io>
2023-10-06 10:48:17 +0200

    Job09

--- Job09/accessrights.sh
+++ Job09/accessrights.sh
@@ -1,20 +1,14 @@
-#!/bin/bash
-
-# Emplacement du fichier CSV (dans le même dossier que le script)
-csv_file="Shell_Userlist.csv"
-
-# Parcours du fichier CSV
-while IFS=',' read -r username role; do
+while IFS=, read -r uid prenon non mdp role; do
 
     if [ "$role" = "admin" ]; then
-        # Créer l'utilisateur avec des privilèges de superutilisateur
-        sudo useradd -m -G sudo "$username"
-        echo "Utilisateur $username créé avec des privilèges de superutilisateur."
-
+        # Création de l'utilisateur avec des privilèges de super utilisateur
+        sudo useradd -m -G sudo "$prenon-$non"
+        # Définition du mot de passe pour l'utilisateur
+        echo "$prenon-$non:$mdp" | sudo chpasswd
     else
-        # Créer un utilisateur normal
-        sudo useradd -m "$username"
-        echo "Utilisateur $username créé."
+        # Création de l'utilisateur sans privilèges de super utilisateur
+        sudo useradd -m "$prenon-$non"
+        # Définition du mot de passe pour l'utilisateur
+        echo "$prenon-$non:$mdp" | sudo chpasswd
     fi
-
-done <"$csv_file"
+done < Shell_Userlist.csv
